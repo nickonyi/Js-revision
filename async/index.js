b1982b1759f6a4cb4d0e441a1ea891a5f7de729d
nickonyi <nickoluoch1130@gmail.com>
2023-10-20 06:33:46 +0300

    promise

--- async/index.js
+++ async/index.js
@@ -1,18 +1,24 @@
-const userLeft = true;
+const userLeft = false;
 const userWatchingCatMeme = false;
 
-function watchTutorialCallBack(callBack,errorCallBack){
-    if(userLeft){
+function watchTutorialCallBack(callBack, errorCallBack) {
+    if (userLeft) {
         errorCallBack({
-            name:'user Left',
-            message:':('
-        }) 
-    }else if (userWatchingCatMeme){
-         errorCallBack({
-            name:'User is watching cat memes',
-            message:'I am < cat memes'
-         })   
+            name: 'user Left',
+            message: ':('
+        })
+    } else if (userWatchingCatMeme) {
+        errorCallBack({
+            name: 'User is watching cat memes',
+            message: 'I am < cat memes'
+        })
     } else {
-        callBack
+        callBack('Thumbs up and subscribe!');
     }
 }
+
+watchTutorialCallBack((message) => {
+    console.log('Success!! ' + message);
+}, (error) => {
+    console.log(error.name + ' ' + error.message);
+})
