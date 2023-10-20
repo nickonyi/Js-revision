d19c2a0b5b10c56e6348916bc59204eca59750c9
nickonyi <nickoluoch1130@gmail.com>
2023-10-20 06:46:03 +0300

    promise

--- async/index.js
+++ async/index.js
@@ -1,24 +1,49 @@
 const userLeft = false;
-const userWatchingCatMeme = false;
+const userWatchingCatMeme = true;
+///
+///function watchTutorialCallBack(callBack, errorCallBack) {
+///    if (userLeft) {
+///        errorCallBack({
+///            name: 'user Left',
+///            message: ':('
+///        })
+///    } else if (userWatchingCatMeme) {
+///        errorCallBack({
+///            name: 'User is watching cat memes',
+///            message: 'I am < cat memes'
+///        })
+///    } else {
+///        callBack('Thumbs up and subscribe!');
+///    }
+///}
+///
+///watchTutorialCallBack((message) => {
+///    console.log('Success!! ' + message);
+///}, (error) => {
+///    console.log(error.name + ' ' + error.message);
+///})
 
-function watchTutorialCallBack(callBack, errorCallBack) {
-    if (userLeft) {
-        errorCallBack({
-            name: 'user Left',
-            message: ':('
-        })
-    } else if (userWatchingCatMeme) {
-        errorCallBack({
-            name: 'User is watching cat memes',
-            message: 'I am < cat memes'
-        })
-    } else {
-        callBack('Thumbs up and subscribe!');
-    }
+function watchTutorialPromise() {
+    return new Promise((resolve, reject) => {
+        if (userLeft) {
+            reject({
+                name: 'user Left',
+                message: ':('
+            })
+        } else if (userWatchingCatMeme) {
+            reject({
+                name: 'User is watching cat memes',
+                message: 'I am < cat memes'
+            })
+        } else {
+            resolve('Thumbs up and subscribe!');
+        }
+    });
+
 }
 
-watchTutorialCallBack((message) => {
+watchTutorialPromise().then(((message) => {
     console.log('Success!! ' + message);
-}, (error) => {
+})).catch((error) => {
     console.log(error.name + ' ' + error.message);
 })
