c6410bc423e668c5cba6e17b06bb23a7e05e63fb
unknown <nickoluoch1130@gmail.com>
2024-08-21 12:15:49 +0300

    some random code

--- object-object-constructors/dap.js
+++ object-object-constructors/dap.js
@@ -12,3 +12,41 @@ function countSheeps(sheep) {
 
     return sum / nums.length;
   }
+
+  function sumDigits(number) {
+    const absoluteVal = Math.abs(number);
+    const stringVal = absoluteVal.toString();
+    let sumDigit = 0;
+
+    for (const digit of stringVal) {
+         sumDigit += parseInt(digit,10)
+    }
+
+    return sumDigit;
+  }
+
+
+  function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    const parseDate = (date) => new Date(date);  
+    
+    if (enteredCode !== correctCode){
+        return false;
+      } else {
+        const expirationDateStr = parseDate(expirationDate);
+        const currentDateStr = parseDate(currentDate);
+
+        return currentDateStr <= expirationDateStr;
+      }
+  }
+
+
+  console.log(checkCoupon("123", "123", "July 9, 2015", "July 9, 2015") );
+  console.log(checkCoupon("123", "123", "July 9, 2015", "July 2, 2015"));
+
+  function dutyFree(normPrice, discount, hol){
+    discount = discount /100;    
+    const savings = normPrice * discount;
+    return Math.floor(hol/savings);
+  }
+
+  console.log(dutyFree(24,35,3000));
