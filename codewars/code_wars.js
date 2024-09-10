211c6e1b21726685c46d78c3c8c56a38e0d1c830
phatommobb <nickoluoch1130@gmail.com>
2024-09-10 19:11:08 +0300

    code

---
+++ codewars/code_wars.js
@@ -0,0 +1,36 @@
+function replace(s){
+    //coding and coding....
+    return s.replace(/[aeiouAEIOU]/g,"!");
+    
+  }
+
+
+  function multiTable(number) {
+    // good luck
+    let table = "";
+    for (let i = 1; i <=10; i++) {
+        table += `${i} * ${number} = ${i*number}\n`;        
+    }
+
+    return table.trim();
+  };
+
+ 
+
+  
+ 
+  
+  function take(arr, n) {
+    // Your code here
+    return arr.slice(0,n);
+  }
+
+  
+
+  function position(letter){
+    //Write your own Code!
+      const letterPos = letter.charCodeAt(0) - 96; 
+      return `Position of alphabet: ${letterPos}`;
+    }
+    console.log(position('c'));
+    
