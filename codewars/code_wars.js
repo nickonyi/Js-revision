72f3ed9c02286daec4d897f3caed39cf63199c93
phatommobb <nickoluoch1130@gmail.com>
2024-10-13 15:50:05 +0300

    code

--- codewars/code_wars.js
+++ codewars/code_wars.js
@@ -41,3 +41,16 @@ function replace(s){
     }
 
     const REGEXP = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]{6,}$/;
+
+    function factorial(n) {
+      if (n < 0) {
+        return "Factorial is not defined for negative numbers";
+      }
+      if (n === 0 || n === 1) {
+        return 1;
+      }
+      return n * factorial(n - 1);
+    }
+    
+    console.log(factorial(50)); // Output: 120
+    
