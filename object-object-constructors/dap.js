9cae32c107f96cd08e565d44185cab18070a3368
unknown <nickoluoch1130@gmail.com>
2024-10-24 14:38:04 +0300

    code

--- object-object-constructors/dap.js
+++ object-object-constructors/dap.js
@@ -309,4 +309,8 @@ function noOdds( values ){
   return values.filter((num) => num % 2 === 0);
 }
 
-console.log(noOdds([0,1,2,3,4]));
+const stringToNumber = function(str){
+  // put your code here
+  return Number(str);
+}
+
