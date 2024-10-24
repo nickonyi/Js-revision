84ad51be69c3349d1c4eb736b5198348e8cbbcd0
unknown <nickoluoch1130@gmail.com>
2024-10-24 14:42:42 +0300

    code

--- object-object-constructors/dap.js
+++ object-object-constructors/dap.js
@@ -314,3 +314,11 @@ const stringToNumber = function(str){
   return Number(str);
 }
 
+function chromosomeCheck(sperm) {
+     if(sperm === "XX"){
+      return  "Congratulations! You're going to have a daughter.";
+     } else if (sperm === "XY"){
+       return "Congratulations! You're going to have a son.";
+     }
+}
+
