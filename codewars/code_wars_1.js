e92effce830f379740e02eb455de9bf4131d8ed0
phatommobb <nickoluoch1130@gmail.com>
2024-11-01 22:35:50 +0300

    code

--- codewars/code_wars_1.js
+++ codewars/code_wars_1.js
@@ -146,3 +146,29 @@ function move(position,roll){
   return position + (roll * 2);
 }
 
+function getLength(arr){
+  //return length of arr
+  return arr.length;
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[i];
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length - 1]
+}
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+  arr.push(el);
+  
+  return arr
+}
+function popElement(arr){
+  //pop an element from arr
+    arr.pop()
+  return arr;
+}
+
+console.log(pushElement([]));
