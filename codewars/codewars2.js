06023701894116ab364b7fc83cb8153934d2fcc9
phatommobb <nickoluoch1130@gmail.com>
2024-12-16 19:38:55 +0300

    code

--- codewars/codewars2.js
+++ codewars/codewars2.js
@@ -79,3 +79,9 @@ function getChar(c) {
   // ...
   return String.fromCharCode(c);
 }
+
+function noSpace(x) {
+  return x.replace(/\s+/g, '');
+}
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
