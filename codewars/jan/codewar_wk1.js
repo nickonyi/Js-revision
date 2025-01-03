30d80b3fce5fa0fa69b11cf5836eed3c3dc70de3
phatommobb <nickoluoch1130@gmail.com>
2025-01-04 00:40:00 +0300

    code

--- codewars/jan/codewar_wk1.js
+++ codewars/jan/codewar_wk1.js
@@ -6,7 +6,7 @@ multiplicationTable = function (size) {
 
   for (let i = 0; i < row; i++) {
     multArr[i] = [];
-
+    //To store the best pair found
     for (let j = 0; j < col; j++) {
       multArr[i][j] = (i + 1) * (j + 1);
     }
@@ -16,5 +16,37 @@ multiplicationTable = function (size) {
 };
 
 function sumPairs(ints, s) {
-  return [0, 0] || undefined;
+  let bestPair = null;
+  let bestSecondIndex = Infinity;
+
+  for (let i = 0; i < ints.length; i++) {
+    for (let j = i + 1; j < ints.length; j++) {
+      if (ints[i] + ints[j] === s) {
+        if (j < bestSecondIndex) {
+          bestPair = [ints[i], ints[j]];
+          bestSecondIndex = j;
+        }
+      }
+    }
+  }
+  if (bestPair !== null) {
+    return bestPair;
+  }
+  return undefined;
 }
+
+//console.log(sumPairs([0, 2, 0], 0));
+//console.log(sumPairs([1, 4, 8, 7, 3, 15], 8));
+
+//console.log(sumPairs([10, 5, 2, 3, 7, 5], 10));
+
+var sum_pairs = function (ints, s) {
+  var seen = {};
+  for (var i = 0; i < ints.length; ++i) {
+    if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
+    seen[ints[i]] = true;
+  }
+};
+
+console.log(sum_pairs([10, 5, 2, 3, 7, 5], 10));
+console.log(sum_pairs([20, -13, 40], -7));
