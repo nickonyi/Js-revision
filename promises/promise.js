7822c9f8d1c2efd0f7744b8f683fa8489c2795b9
phatommobb <nickoluoch1130@gmail.com>
2025-01-21 19:59:08 +0300

    some code

--- promises/promise.js
+++ promises/promise.js
@@ -1,64 +1,77 @@
-const p = new Promise((resolve, reject) => {
-  const a = 1 + 1;
-
-  if (a == 3) {
-    resolve('Successfully resolved');
-  } else {
-    reject('You have been rejected!');
-  }
-});
-
-p.then((message) => {
-  console.log(message);
-}).catch((message) => {
-  console.log(message);
-});
-
-const userLeft = true;
-const userWatchingCatMeme = true;
-
-const watchTutorialCallBack = (callback, errorCallback) => {
-  if (userLeft) {
-    errorCallback({ name: 'userleft', message: ':(' });
-  } else if (userWatchingCatMeme) {
-    errorCallback({
-      name: 'user watching cat meme',
-      message: 'Web dev simplified < cat',
-    });
-  } else {
-    callback('Thumbs up and subscribe');
-  }
-};
-
-//watchTutorialCallBack(
-//  (message) => {
-//    console.log('Success' + message);
-//  },
-//  (error) => {
-//    console.log(error.name + ' ' + error.message);
+//const p = new Promise((resolve, reject) => {
+//  const a = 1 + 1;
+//
+//  if (a == 3) {
+//    resolve('Successfully resolved');
+//  } else {
+//    reject('You have been rejected!');
 //  }
-//);
+//});
+//
+//p.then((message) => {
+//  console.log(message);
+//}).catch((message) => {
+//  console.log(message);
+//});
+//
+//const userLeft = true;
+//const userWatchingCatMeme = true;
+//
+//const watchTutorialCallBack = (callback, errorCallback) => {
+//  if (userLeft) {
+//    errorCallback({ name: 'userleft', message: ':(' });
+//  } else if (userWatchingCatMeme) {
+//    errorCallback({
+//      name: 'user watching cat meme',
+//      message: 'Web dev simplified < cat',
+//    });
+//  } else {
+//    callback('Thumbs up and subscribe');
+//  }
+//};
+//
+////watchTutorialCallBack(
+////  (message) => {
+////    console.log('Success' + message);
+////  },
+////  (error) => {
+////    console.log(error.name + ' ' + error.message);
+////  }
+////);
+//
+//const watchTutorialCallBackPromise = () => {
+//  return new Promise((resolve, reject) => {
+//    if (userLeft) {
+//      reject({ name: 'userleft', message: ':(' });
+//    } else if (userWatchingCatMeme) {
+//      reject({
+//        name: 'user watching cat meme',
+//        message: 'Web dev simplified < cat',
+//      });
+//    } else {
+//      resolve('Thumbs up and subscribe');
+//    }
+//  });
+//};
+//
+//watchTutorialCallBackPromise()
+//  .then((message) => {
+//    console.log('Uwe wangu' + message);
+//  })
+//  .catch((error) => {
+//    console.log(error.message);
+//  });
+//console.log('call');
 
-const watchTutorialCallBackPromise = () => {
-  return new Promise((resolve, reject) => {
-    if (userLeft) {
-      reject({ name: 'userleft', message: ':(' });
-    } else if (userWatchingCatMeme) {
-      reject({
-        name: 'user watching cat meme',
-        message: 'Web dev simplified < cat',
-      });
-    } else {
-      resolve('Thumbs up and subscribe');
-    }
-  });
-};
+fetch(
+  'https://api.giphy.com/v1/gifs/translate?api_key=qmrhAKpgwN0v2LnG2elF7Tt8eyRLVRxB&s=cats',
+  {
+    mode: 'cors',
+  }
+).then((response) => {
+  console.log(response);
 
-watchTutorialCallBackPromise()
-  .then((message) => {
-    console.log('Uwe wangu' + message);
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
-console.log('call');
+  return response;
+});
+
+console.log('some data');
