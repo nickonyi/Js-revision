a043daae54af1c092d21e7c12a8a6a9fe02973b3
unknown <nickoluoch1130@gmail.com>
2025-02-03 23:20:21 +0300

    wolefer

--- async/app.js
+++ async/app.js
@@ -1,7 +1,7 @@
 const img = document.querySelector('img');
 
 const getCats = async() => {
-    const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=kwHmUl2CEJmAceCfk9krMC9Yhbgn5rdZ&s=cats",{mode:'cors'});
+    const response = await fetch("https://api.gipdggssshy.com/v1/gifs/translate?api_key=kwHmUl2CEJmAceCfk9krMC9Yhbgn5rdZ&s=cats",{mode:'cors'});
     const catData = await response.json();
     img.src = catData.data.images.original.url;
 }
