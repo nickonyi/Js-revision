b843fe6cb01076b56ae6985dfab79884fea0f747
unknown <nickoluoch1130@gmail.com>
2025-03-02 18:07:12 +0300

    ze code

--- codewars/jan/code.js
+++ codewars/jan/code.js
@@ -1,4 +1,9 @@
 function validateUsr(username) {
     const res = /^[a-z0-9_]{4,16}$/;
-    return res;
+    return res.test(username);
+  }
+
+  function reverse(string){
+    //your code here
+    return string.split(" ").reverse().join(" ");
   }
